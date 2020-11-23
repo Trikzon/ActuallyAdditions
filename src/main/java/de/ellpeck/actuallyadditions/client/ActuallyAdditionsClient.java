f6aebbbbac3105bfdcaec5e3db2bf215f5cccaa0
Trikzon <trikzondev@gmail.com>
2020-11-23 15:31:50 -0500

    Beginning of a journey to fabric

---
+++ src/main/java/de/ellpeck/actuallyadditions/client/ActuallyAdditionsClient.java
@@ -0,0 +1,10 @@
+package de.ellpeck.actuallyadditions.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ActuallyAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/de/ellpeck/actuallyadditions/common/ActuallyAdditions.java
+++ src/main/java/de/ellpeck/actuallyadditions/common/ActuallyAdditions.java
@@ -1,48 +1,16 @@
 package de.ellpeck.actuallyadditions.common;
 
-import de.ellpeck.actuallyadditions.common.blocks.ActuallyBlocks;
-import de.ellpeck.actuallyadditions.common.items.ActuallyItems;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(ActuallyAdditions.MOD_ID)
-public class ActuallyAdditions {
+public class ActuallyAdditions implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static final String MOD_ID = "actuallyadditions";
 
-    public static final ItemGroup ACTUALLY_GROUP = new ItemGroup(MOD_ID) {
-        @Override
-        public ItemStack createIcon() {
-            return new ItemStack(ActuallyItems.BOOKLET.get());
-        }
-    };
-
-    public ActuallyAdditions() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ActuallyBlocks.BLOCKS.register(eventBus);
-        ActuallyItems.ITEMS.register(eventBus);
-
-        eventBus.addListener(this::setup);
-        eventBus.addListener(this::clientSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(FMLCommonSetupEvent event) {
-
-    }
-
-    private void clientSetup(FMLClientSetupEvent event) {
+    @Override
+    public void onInitialize() {
 
     }
 }
